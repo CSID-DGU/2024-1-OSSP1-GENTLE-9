3a7435cee8c35238fb30c3d167babb2dad39fa15
memymini <minhi@gmail.com>
2024-04-12 19:46:06 +0900

    [feat]소개페이지 작성

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -13,9 +14,11 @@ function Home() {
           <br />N<span className={styles.highlight}>E</span>WS SERVICE
         </p>
       </div>
-      <div className={styles.button}>
-        <p>기사 생성하기</p>
-      </div>
+      <Link to="/create" style={{ textDecoration: "none", color: "black" }}>
+        <div className={styles.button}>
+          <p>기사 생성하기</p>
+        </div>
+      </Link>
     </div>
   );
 }
