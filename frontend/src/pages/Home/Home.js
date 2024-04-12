cb55d890a7c7b316dd1a36822e3b5783df0f4f5d
memymini <minhi@gmail.com>
2024-04-12 17:55:37 +0900

    [feat]Home 구현

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "./Home.module.css";
 
 function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <p>
+          <span className={styles.highlight}>GE</span>NERATE{" "}
+          <span className={styles.highlight}>N</span>EU
+          <span className={styles.highlight}>T</span>RA
+          <span className={styles.highlight}>L</span>
+          <br />N<span className={styles.highlight}>E</span>WS SERVICE
+        </p>
+      </div>
+      <div className={styles.button}>
+        <p>기사 생성하기</p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
