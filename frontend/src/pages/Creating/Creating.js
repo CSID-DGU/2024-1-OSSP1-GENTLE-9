6afbe0c3bed96d331226c718e9cc13094323eace
memymini <minhi@gmail.com>
2024-06-17 23:23:24 +0900

    [fix]creating 수정

--- frontend/src/pages/Creating/Creating.js
+++ frontend/src/pages/Creating/Creating.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./Creating.module.css";
 import creating from "../../assets/images/creating.png";
+import FadeLoader from "react-spinners/FadeLoader";
 
 function Creating() {
   return (
     <div className={styles.container}>
-      <img src={creating} alt="creating" />
+      <FadeLoader color="#000000" />
       <p className={styles.textAnimation}>기사 생성 중...</p>
     </div>
   );
